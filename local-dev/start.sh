3498b41afdeb4c28669f26a3333063020b66c420
Vladislav Shub <1196390+vladshub@users.noreply.github.com>
2019-06-28 20:51:55 +0300

    fixed kube deployment to include ingress and added a local setup for test

---
+++ local-dev/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+git clone https://github.com/hashicorp/consul-helm.git
+
+helm init
+
+helm upgrade --install nginx-ingress stable/nginx-ingress --set controller.hostNetwork=true
+helm upgrade --install dashboard stable/kubernetes-dashboard -f ./dashboard.yaml
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep default-token | awk '{print $1}')
+helm upgrade --install consul ./consul-helm -f ./consul_values.yaml
+
+kubectl apply -f ./consul_ingres.yaml
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  labels:
+    addonmanager.kubernetes.io/mode: EnsureExists
+  name: kube-dns
+  namespace: kube-system
+data:
+  stubDomains: |
+    {"consul": ["$(kubectl get svc consul-consul-dns -o jsonpath='{.spec.clusterIP}')"]}
+EOF
+
+kubectl apply -f ./echo.yaml
+kubectl apply -f ../k8s/
